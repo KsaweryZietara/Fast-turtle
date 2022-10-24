b8a3d63353bd8cebcbb54751b56f2214cdd89383
KsaweryZietara <91937141+KsaweryZietara@users.noreply.github.com>
2022-10-24 19:48:54 +0200

    Add files via upload

---
+++ src/main/java/com/example/fastturtle/FastTurtleApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastturtle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastTurtleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastTurtleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fastturtle/FastTurtleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastturtle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastTurtleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
