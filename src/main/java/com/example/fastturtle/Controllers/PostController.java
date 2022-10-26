037cf59ac13b157c7d3c5579d2b94a2eaed013f9
KsaweryZietara <Zietara.Ksawery@gmail.com>
2022-10-26 22:32:53 +0200

    Comment controller

--- src/main/java/com/example/fastturtle/Controllers/PostController.java
+++ src/main/java/com/example/fastturtle/Controllers/PostController.java
@@ -9,7 +9,9 @@ import com.example.fastturtle.Models.User;
 import com.example.fastturtle.Repositories.PostRepository;
 import com.example.fastturtle.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/post/")
@@ -42,6 +46,29 @@ public class PostController {
                 post.getUser().getLastName(),
                 post.getCreationTime(),
                 post.getContent());
+    }
+
+    @GetMapping("profile/{id}/{page}")
+    public List<ReturnPostDto> getPostsByUserId(@PathVariable Long id, @PathVariable int page){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+
+        Pageable pageable = PageRequest.of(page, 2, Sort.by("creationTime"));
+        //User user = new User(id);
+
+        List<Post> l = postRepository.findAllByUserId(id, pageable);
+        ArrayList<ReturnPostDto> posts = new ArrayList<>();
+
+        for (Post p : l){
+            posts.add(new ReturnPostDto(p.getId(),
+                    p.getUser().getFirstName(),
+                    p.getUser().getLastName(),
+                    p.getCreationTime(),
+                    p.getContent()));
+        }
+
+        return posts;
     }
 
     @PostMapping
--- src/main/java/com/example/fastturtle/Repositories/PostRepository.java
+++ src/main/java/com/example/fastturtle/Repositories/PostRepository.java
@@ -1,8 +1,12 @@
 package com.example.fastturtle.Repositories;
 
 import com.example.fastturtle.Models.Post;
+import com.example.fastturtle.Models.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
+import java.util.List;
 
+public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
+    public List<Post> findAllByUserId(Long id, Pageable pageable);
 }
